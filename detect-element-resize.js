daaee4414fce661190e1617605cbea949d519257
Jason Swartout <jason@stratusdata.com>
2014-10-02 15:31:05 -0400

    Prevent scrollbars from displaying in webkit

--- detect-element-resize.js
+++ detect-element-resize.js
@@ -61,7 +61,7 @@
 	function createStyles() {
 		if (!stylesCreated) {
 			//opacity:0 works around a chrome bug https://code.google.com/p/chromium/issues/detail?id=286360
-			var css = '.resize-triggers { visibility: hidden; opacity: 0; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }',
+			var css = '.resize-triggers { visibility: hidden; opacity: 0; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; } .resize-triggers::-webkit-scrollbar, .contract-trigger::-webkit-scrollbar, .resize-triggers > div::-webkit-scrollbar { display: none; }',
 				head = document.head || document.getElementsByTagName('head')[0],
 				style = document.createElement('style');
 
