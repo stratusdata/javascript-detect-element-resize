f9676e9b9f1af7563cc0971765df30192cc26321
Jason Swartout <jason@stratusdata.com>
2014-10-02 15:39:27 -0400

    Prevent re-positioning BODY element

--- jquery.resize.js
+++ jquery.resize.js
@@ -98,7 +98,7 @@
 		if (attachEvent) element.attachEvent('onresize', fn);
 		else {
 			if (!element.__resizeTriggers__) {
-				if (getComputedStyle(element).position == 'static') element.style.position = 'relative';
+				if (getComputedStyle(element).position == 'static' && document.body !== element) element.style.position = 'relative';
 				createStyles();
 				element.__resizeLast__ = {};
 				element.__resizeListeners__ = [];
